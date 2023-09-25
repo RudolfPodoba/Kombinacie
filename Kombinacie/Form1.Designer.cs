bf38ce77c432b5966016e0dc218a794659b8ff35
RudolfPodoba <podobar@gmail.com>
2023-09-25 18:42:44 +0200

    Add project files.

---
+++ Kombinacie/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Kombinacie
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 104);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Vypíš";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(102, 52);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(170, 253);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 52);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(75, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "5";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 78);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(75, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "3";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Popis";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 308);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(175, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Počet kombinácií bez opakovania: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 326);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Kombiácie bez opakovania";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Kombinacie/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kombinacie
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private int vypis_kombinacie(int i_k , int[] i_kombinacia, int i_pocet_kombinacii)
+        {
+            string s_kombinacia = "{";
+            int i_vysledok = i_pocet_kombinacii;
+
+            // vypísanie kombinácie
+            for (int i = 0; i < i_k; i++)
+            {
+                s_kombinacia += i_kombinacia[i].ToString();
+                if (i < (i_k - 1))
+                {
+                    s_kombinacia += ", ";
+                }
+            }
+
+            s_kombinacia += "}";
+
+            richTextBox1.AppendText(s_kombinacia + Environment.NewLine);
+            i_vysledok++;
+
+            return i_vysledok;
+        }
+        
+        private int faktorial(int i_cislo)
+        {
+            int i_vysledok = i_cislo;
+
+            for (int i = i_vysledok - 1; i > 0; i--)
+            {
+                i_vysledok *= i;
+            }
+
+            return i_vysledok;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // na námet, resp. inšpirácia z https://stackoverflow.com/questions/8316479/combination-without-repetition-of-n-elements-without-use-for-to-do
+            // výsledok je možné overiť na https://www.omnicalculator.com/statistics/combinations-without-repetition
+            // definovanie premenných a načítanie
+            int i_n = 0;
+            int i_k = 0;
+            int i_i = 1;
+            int i_j = 1;
+            int i_pocet_kombinacii = 0;
+            int i_pocet_kombinacii_vypocet = 0;
+            
+            // načítanie zadaných hodnôt
+            Int32.TryParse(textBox1.Text, out i_n);
+            Int32.TryParse(textBox2.Text, out i_k);
+
+            // nastavenie dĺžky pola pre kombinácie a jeho definovanie
+            int[] komb = new int[i_k];
+
+            // vyčistenie priestoru pre zapisovanie
+            richTextBox1.Clear();
+
+            // naplnenie pola prvou kombináciou
+            for (int i = 0; i < i_k; i++)
+            {
+                komb[i] = i + 1;
+            }
+            
+            // vypísanie kombinácie
+            i_pocet_kombinacii = vypis_kombinacie(i_k, komb, i_pocet_kombinacii);
+
+            if (i_k > 0 && i_k < i_n)
+            {
+                do
+                {
+                    if (komb[i_k - 1] < i_n) // ak sa posledné číslo kombinácie ešte dá zvýšiť
+                    {
+                        // zvýšenie posledného čísla kombinácie
+                        komb[i_k - 1]++;
+
+                        // vypísanie kombinácie
+                        i_pocet_kombinacii = vypis_kombinacie(i_k, komb, i_pocet_kombinacii);
+                    }
+                    else // aj je posledné číslo kombinácie maximálne možné
+                    {
+                        i_i = 1;
+                        // nájde najbližšie číslo kombinácie, ktoré sa dá ešte môže zvýšiť
+                        while (komb[(i_k - 1) - i_i] == i_n - i_i)
+                        {
+                            i_i++;
+                        }
+
+                        // zvýši nájdené číslo kombinácie
+                        komb[(i_k - 1) - i_i]++;
+
+                        i_j = 1;
+                        // vytvorí ďalši kombináciu; každé ďalšie číslo kombinácie bude o jedno vyššie, ako predchádzajúce
+                        while (((i_k - 1) - i_i) + i_j <= (i_k - 1))
+                        {
+                            komb[((i_k - 1) - i_i) + i_j] = komb[(i_k - 1) - i_i] + i_j;
+                            i_j++;
+                        }
+                        // vypísanie kombinácie
+                        i_pocet_kombinacii = vypis_kombinacie(i_k, komb, i_pocet_kombinacii);
+                    }
+                } while (komb[0] <= (i_n - i_k));
+
+                // kontrola počtu nájdených kombinácií Ck(n) = n! / (k! * (n - k)!) - https://www.hackmath.net/sk/kalkulacka/kombinacie-a-permutacie
+                i_pocet_kombinacii_vypocet = faktorial(i_n) / (faktorial(i_k) * faktorial(i_n - i_k));
+
+                if (i_pocet_kombinacii_vypocet == i_pocet_kombinacii)
+                {
+                    // vypíše počet nájdených kombinácií                
+                    label2.Text = "Počet kombinácií bez opakovania: " + i_pocet_kombinacii.ToString();
+                }
+                else
+                {
+                    // vypíše chybu
+                    label2.Text = "CHYBA PRI VÝPOČTE!!!";
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "Aplikácia vypíše všetky možnosti výberu 'k' prvok " + Environment.NewLine;
+            label1.Text += "z 'n' prvkovej množiny kladných celých čísel " + Environment.NewLine + "bez opakovania.";
+
+            label2.Text = "Počet kombinácií bez opakovania: ";
+        }
+    }
+}
